7ed3b4d77478aa133d2d327533cfd2af9f533d30
TaimurHasan <taimurhasan11@gmail.com>
2022-07-11 20:44:09 -0400

    editing footer nested flexbox

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,13 +4,11 @@ import classes from "./Footer.module.css"
 function Footer() {
     return (
         <div className = {classes.Footer}>
-            <div className = {classes.FootNote}>
                 <a className={classes.EmailButton} href="mailto:taimurhasan11@gmail.com?subject=Lets%20Connect!">
                     <button>Contact Me</button>
                 </a>            
                 <p>Built with React &trade; </p>
                 <p >&copy; 2022</p>
-            </div>
         </div>
     )
 };
