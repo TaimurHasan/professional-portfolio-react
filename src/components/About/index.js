e06fb622563288d0f227b63cdba815422926a580
TaimurHasan <taimurhasan11@gmail.com>
2022-09-30 16:41:42 -0400

    slight header font change for mobile and project card rounding

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,12 +13,12 @@ function About({ setCurrentSection, styles }) {
                     Frankly, it became the very thing that brought me to work everyday. <br/><br/>
                     I love everything about business, including the ability to collaborate with others, work on consumer-facing products, and make a lasting impact beyond financials.
                     Even then, nothing ever beat the feeling I got when I was able to streamline a process and deliver the solution to a happy team. <br/><br/> 
-                    <span style={{fontStyle:"italic", fontWeight:"bold"}}> It all started with Excel </span>. I'm talking basic Excel; IF statements, VLOOKUPs, PIVOT tables - all that fun stuff! 
+                    <span style={{fontStyle:"italic", fontWeight:"bold"}}> It all started with Excel</span>. I'm talking basic Excel; IF statements, VLOOKUPs, PIVOT tables - all that fun stuff! 
                     From there, I got involved with more complex Excel formulas, Macros, VBA, and automation. It engaged me, kept me motivated, and got me recognized. 
                     I felt like I could never stop learning.<br/><br/>
                     That exact feeling is what pushed me to expand my learnings beyond just a spreadsheet software program. With that, I jumped head first into the world of programming, coding, and computer science.
                     <br/><br/>
-                    I am now proud to call myself a Full Stack Developer, devoted to learning and solving the most challenging problems. In this portfolio, you will find my journey in the world of Full Stack Development.
+                    I am now proud to call myself a <span style={{color: "hsla(31, 90%, 76%, 1)", fontWeight:"bold"}}> Full Stack Engineer</span>, devoted to learning and solving the most challenging problems. In this portfolio, you will find my journey in the world of software.
                     <br/><br/>
                 </p>
                 <div className={classes.NavBtns}>
